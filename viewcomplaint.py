48f714ebea70e9b80f2a353edd0765e8c356d152
aliadnan2000 <60284195+aliadnan2000@users.noreply.github.com>
2022-10-08 23:17:18 +0500

    Delete viewcomplaint.py

--- viewcomplaint.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, url_for, request, session, redirect
-from app import app
-
-@app.route('/viewcom')
-def reset_pass():
-    return render_template('viewcomplaint.html')
-
-
-
