030be4da61caa0ca8ce6f3ee9135fbb3eda7953d
aliadnan2000 <60284195+aliadnan2000@users.noreply.github.com>
2022-10-08 23:18:45 +0500

    Add files via upload

--- Web App/venv/assigning.py
+++ Web App/venv/assigning.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from app import app
 
-@app.route('/viewcom')
+@app.route('/assign')
 def reset_pass():
-    return render_template('viewcomplaint.html')
-
-
-
+    return render_template('assigningcomplaint.html')
--- Web App/venv/complaintdetails.py
+++ Web App/venv/complaintdetails.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from app import app
 
-@app.route('/viewcom')
+@app.route('/comdetails')
 def reset_pass():
-    return render_template('viewcomplaint.html')
-
-
-
+    return render_template('complaintdetails.html')
