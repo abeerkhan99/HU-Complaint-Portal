41f65e32ab59ea391ebb9967c3aad29db5106397
Abeer Khan <54454858+abeerkhan99@users.noreply.github.com>
2022-10-11 14:49:23 +0500

    Delete makecomplaint.py

--- Web App/venv/makecomplaint.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, url_for, request, session, redirect
-from app import app
-
-@app.route('/makecomp')
-def login():
-    return render_template('makecomplaint.html')
