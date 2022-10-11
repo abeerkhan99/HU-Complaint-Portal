b042a098802c12fee687702c887b04cf042761a1
Abeer Khan <54454858+abeerkhan99@users.noreply.github.com>
2022-10-11 14:49:35 +0500

    Delete assigning.py

--- Web App/venv/assigning.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, url_for, request, session, redirect
-from app import app
-
-@app.route('/assign')
-def reset_pass():
-    return render_template('assigningcomplaint.html')
