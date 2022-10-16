5271c2fa905c56ff535882abe05fcb2d48f95269
Abeer Khan <54454858+abeerkhan99@users.noreply.github.com>
2022-10-16 09:49:48 +0500

    Add files via upload

--- Web App/venv/app.py
+++ Web App/venv/app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, url_for, request, session, redirect
-
+import psycopg2
 
 # app instance
 app = Flask(__name__, template_folder='Template')
 # for sessions
 app.secret_key = 'BAD_SECRET_KEY'
+
+def get_db_connection():
+    conn = psycopg2.connect(
+            host="localhost",
+            database="hu-complaint-portal",
+            user= 'postgres',
+            password= '12345')
+    
+    print("YES")
+    return conn
+
+# Open a cursor to perform database operations
+conn = get_db_connection()
+cur = conn.cursor()
 
 # routing
 @app.route('/')
--- Web App/venv/login.py
+++ Web App/venv/login.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from app import app
+from app import conn, cur
 
 @app.route('/login')
 def login():
@@ -9,7 +10,88 @@ def login():
 @app.route('/submit', methods = ['GET', 'POST'])
 def submit():
     if request.method == 'POST':
-        return redirect('/student-dashboard')
+
+        uemail = request.form['email']
+        upassword = request.form['password']
+
+        print('PASS: ', upassword)
+        print('EMAIL:', uemail)
+
+        # check if it's student email or admin email
+        if '@st.habib.edu.pk' in uemail:
+            #student email
+
+            # check if email exists in database
+            cur.execute('select StudentEmail from Student where StudentEmail = %s', (uemail,))
+            student_e = cur.fetchall()
+
+            if len(student_e) != 0:
+                # if email exists in database
+                # then check if email and password combination match
+
+                cur.execute('select StudentPassword from Student where StudentEmail = %s', (uemail,))
+
+                student_p = cur.fetchall()
+
+                if student_p[0][0] == str(upassword):
+                    print("SUCCESS")
+                    
+                    cur.execute('select StudentFirstName, StudentLastName, StudentID, ProgramName from Student, Program where StudentEmail = %s and ProgramID = Student_Program', (uemail,))
+
+                    student_info = cur.fetchall()
+                    print(student_info)
+
+                    #sessions
+                    session['student_id'] = student_info[0][2]
+                    session['student_fname'] = student_info[0][0]
+                    session['student_lname'] = student_info[0][1]
+                    session['student_program'] = student_info[0][3]
+
+                    return redirect('/student-dashboard')
+                else:
+                    print('FAIL')
+                    return render_template('login.html', message = "Incorrect password")
+            else:
+                print('FAIL')
+                return render_template('login.html', message = "Incorrect email")
+
+        else:
+            # admin email
+
+            # check if email exists in database
+            cur.execute('select AdminEmail from Admin where AdminEmail = %s', (uemail,))
+            admin_e = cur.fetchall()
+
+            if len(admin_e) != 0:
+                # if email exists in database
+                # then check if email and password combination match
+
+                cur.execute('select AdminPassword from Admin where AdminEmail = %s', (uemail,))
+
+                admin_p = cur.fetchall()
+
+                if admin_p[0][0] == str(upassword):
+                    print("SUCCESS")
+                    
+                    cur.execute('select AdminFirstName, AdminLastName, AdminID, DepartmentName from Admin, Department where AdminEmail = %s and DepartmentID = Admin_Department', (uemail,))
+
+                    admin_info = cur.fetchall()
+                    print(admin_info)
+
+                    #sessions
+                    session['admin_id'] = admin_info[0][2]
+                    session['admin_fname'] = admin_info[0][0]
+                    session['admin_lname'] = admin_info[0][1]
+                    session['admin_department'] = admin_info[0][3]
+                    
+                    return redirect('/admin-dashboard')
+                else:
+                    print('FAIL')
+                    return render_template('login.html', message = "Incorrect password")
+            else:
+                print('FAIL')
+                return render_template('login.html', message = "Incorrect email")
+
     else:
         return render_template('login.html')
 
